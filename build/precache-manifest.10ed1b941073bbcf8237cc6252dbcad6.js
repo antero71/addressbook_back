8273969662b311e4120010461689c1b99a50f26e
Antero Oikkonen <antero.oikkonen@gmail.com>
2019-09-08 22:18:58 +0300

    add static front

---
+++ build/precache-manifest.10ed1b941073bbcf8237cc6252dbcad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0d323315733f577289c15d9bf716b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62bcc7d335f030191d6c",
+    "url": "/static/css/main.13c959f9.chunk.css"
+  },
+  {
+    "revision": "787e8e4f8512485700f3",
+    "url": "/static/js/2.7183225e.chunk.js"
+  },
+  {
+    "revision": "62bcc7d335f030191d6c",
+    "url": "/static/js/main.18f94a18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
