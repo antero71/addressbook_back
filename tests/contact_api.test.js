cc1bbf46b32ecbea79d525fa8de6e199cf3bf1c5
Antero Oikkonen <antero.oikkonen@gmail.com>
2019-09-08 16:28:20 +0300

    new tests

--- tests/contact_api.test.js
+++ tests/contact_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
+const Contact = require('../models/contact')
 
 
 const api = supertest(app)
@@ -70,7 +71,7 @@ test('a valid contact can be added ', async () => {
   const names = contactsAtEnd.map(r => r.name)
   const phones = contactsAtEnd.map(r => r.phone)
 
-  expect(contactsAtEnd).toBe(helper.initialContacts.length + 1)
+  expect(contactsAtEnd.length).toBe(helper.initialContacts.length + 1)
   expect(names).toContain(
     'Lasse'
   )
@@ -96,7 +97,39 @@ test('contact without name is not added', async () => {
   }
   const response = await api.get('/api/contacts')
 
-  expect(response.body.length).toBe(initialNotes.length)
+  expect(response.body.length).toBe(helper.initialContacts.length)
+})
+
+test('a specific contact can be viewed', async () => {
+  const contactsAtStart = await helper.contactsInDb()
+
+  const contactToView = contactsAtStart[0]
+
+  const resultcontact = await api
+    .get(`/api/contacts/${contactToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  expect(resultcontact.body).toEqual(contactToView)
+})
+
+test('a contact can be deleted', async () => {
+  const contactsAtStart = await helper.contactsInDb()
+  const contactToDelete = contactsAtStart[0]
+
+  await api
+    .delete(`/api/contacts/${contactToDelete.id}`)
+    .expect(204)
+
+  const contactsAtEnd = await helper.contactsInDb()
+
+  expect(contactsAtEnd.length).toBe(
+    helper.initialContacts.length - 1
+  )
+
+  const names = contactsAtEnd.map(r => r.name)
+
+  expect(names).not.toContain(contactToDelete.name)
 })
 
 afterAll(() => {
