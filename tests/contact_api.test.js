73e3928266e2449a024cde3e060821d86e892681
Antero Oikkonen <antero.oikkonen@gmail.com>
2019-09-08 15:38:21 +0300

    api test

--- tests/contact_api.test.js
+++ tests/contact_api.test.js
@@ -1,14 +1,52 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Contact = require('../models/contact')
 
 const api = supertest(app)
+
+const initialContacts = [
+  {
+    name: 'Risto',
+    phone: '040-34342333',
+    address: 'Rataskatu 4, Helsinki'
+  },
+  {
+    name: 'Liisa',
+    email: 'liisa123456778@gmail.com',
+  },
+]
+
+beforeEach(async () => {
+  await Contact.deleteMany({})
+
+  let contactObject = new Contact(initialContacts[0])
+  await contactObject.save()
+
+  contactObject = new Contact(initialContacts[1])
+  await contactObject.save()
+})
+
 
 test('contacts are returned as json', async () => {
   await api
     .get('/api/contacts')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('there are five contacts', async () => {
+  const response = await api.get('/api/contacts')
+
+  expect(response.body.length).toBe(2)
+})
+
+test('the first contact is Risto', async () => {
+  const response = await api.get('/api/contacts')
+
+  console.log('name ',response.body)
+
+  expect(response.body[0].name).toBe('Risto')
 })
 
 afterAll(() => {
